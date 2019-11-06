94d93f3b15279acd8502bb5491340179fe077816
Joice <joicelu007@gmail.com>
2019-11-06 20:51:35 +0800

    updated FileReader

--- src/cma/java/FileReader.java
+++ src/cma/java/FileReader.java
@@ -15,59 +15,55 @@ public class FileReader {
 	public static void main(String[] args) {
 
 	}
-	
+
 	private static List<Tran> readTranfromCSV(String fileName) {
-        List<Tran> trans = new ArrayList<>();
-        Path pathToFile = Paths.get(fileName);
+		List<Tran> trans = new ArrayList<>();
+		Path pathToFile = Paths.get(fileName);
 
-        try (BufferedReader br = Files.newBufferedReader(pathToFile,
-                StandardCharsets.US_ASCII)) {
+		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
 
-            // read the first line from the text file
-            String line = br.readLine();
+			// read the first line from the text file
+			String line = br.readLine();
 
-            // loop until all lines are read
-            while (line != null) {
+			// loop until all lines are read
+			while (line != null) {
 
-                // use string.split to load a string array with the values from
-                // each line of
-                // the file, using a comma as the delimiter
-                String[] attributes = line.split(",");
+				// use string.split to load a string array with the values from
+				// each line of
+				// the file, using a comma as the delimiter
+				String[] attributes = line.split(",");
 
-                Tran tran = createTran(attributes);
+				Tran tran = createTran(attributes);
 
-                // adding book into ArrayList
-                trans.add(tran);
+				// adding Transaction into ArrayList
+				trans.add(tran);
 
-                // read next line before looping
-                // if end of file reached, line would be null
-                line = br.readLine();
-            }
+				// read next line before looping
+				// if end of file reached, line would be null
+				line = br.readLine();
+			}
 
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 
-        return trans;
-    }
-	
+		return trans;
+	}
+
 	private static Tran createTran(String[] metadata) {
-		
-		double stdRate = Double.valueOf(metadata[0]);
-		String baseCCY = metadata[1];
-		String wantedCCY = metadata[2] ;
-		int amount = Integer.valueOf(metadata[3]);
-		String clientType = metadata[4];
-		String time = metadata[5];
-		double finalRate = Double.valueOf(metadata[6]);
-		double profitInWCCY ;
-		double profitInSGD;
-		
-        String name = metadata[0];
-        int price = Integer.parseInt(metadata[1]);
-        String author = metadata[2];
 
-        // create and return book of this metadata
-        return new Tran();
-    }
+		String baseCCY = metadata[0];
+		String wantedCCY = metadata[1];
+		int amount = Integer.valueOf(metadata[2]);
+		String clientType = metadata[3];
+		String time = metadata[4];
+
+		Tran t = new Tran();
+		t.setBaseCCY(baseCCY);
+		t.setWantedCCY(wantedCCY);
+		t.setAmount(amount);
+		t.setClientType(clientType);
+		t.setTime(time);
+		return t;
+	}
 }
