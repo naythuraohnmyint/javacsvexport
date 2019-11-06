3b3ab2e4056854a55cf89b7f35867545de5f9a47
Joice <joicelu007@gmail.com>
2019-11-06 22:12:50 +0800

    update FileReader & add csv

--- src/cma/java/FileReader.java
+++ src/cma/java/FileReader.java
@@ -12,17 +12,68 @@ import java.util.List;
 public class FileReader {
 
 	public static void main(String[] args) {
+		FileReader f = new FileReader();
+		String absolutePath = (Paths.get("").toAbsolutePath().toString());
 
+		List<Rate> rates = f.readRatefromCSV(absolutePath+"\\src\\rate.csv");
+		List<Tran> trans = f.readTranfromCSV(absolutePath+"\\src\\transaction.csv");
+		System.out.println(rates.size());
+		System.out.println(trans.size());
 	}
-	private static int findRate(String base,String wanted,String time){
-	 return 0;	
-	} 
 	
-	private static List<Rate> readRatefromCSV(String fileName){
-		return null;
+	
+	public static List<Rate> readRatefromCSV(String fileName){
+		List<Rate> rates = new ArrayList<>();
+		Path pathToFile = Paths.get(fileName);
+
+		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+
+			// read the first line from the text file
+			String line = br.readLine();
+
+			// loop until all lines are read
+			while (line != null) {
+
+				// use string.split to load a string array with the values from
+				// each line of
+				// the file, using a comma as the delimiter
+				String[] attributes = line.split(",");
+
+				Rate rate = createRate(attributes);
+
+				// adding Transaction into ArrayList
+				rates.add(rate);
+
+				// read next line before looping
+				// if end of file reached, line would be null
+				line = br.readLine();
+			}
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+		return rates;
 	}
 
-	private static List<Tran> readTranfromCSV(String fileName) {
+	public static Rate createRate(String[] metadata) {
+
+		String baseCCY = metadata[0];
+		String wantedCCY = metadata[1];
+		double rate = Double.valueOf(metadata[2]);
+		String time = metadata[3];
+
+		Rate r= new Rate();
+		r.setBaseCCY(baseCCY);
+		r.setWantedCCY(wantedCCY);
+		r.setTime(time);
+		r.setStdRate(rate);
+		
+		r.setTime(time);
+		return r;
+	}
+
+	public static List<Tran> readTranfromCSV(String fileName) {
 		List<Tran> trans = new ArrayList<>();
 		Path pathToFile = Paths.get(fileName);
 
@@ -56,7 +107,7 @@ public class FileReader {
 		return trans;
 	}
 
-	private static Tran createTran(String[] metadata) {
+	public static Tran createTran(String[] metadata) {
 
 		String baseCCY = metadata[0];
 		String wantedCCY = metadata[1];
